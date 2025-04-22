a1daae30d274326a58db1d76245d1a99df12b7b7
McKenzie Skrastins <mskrast1@cs.binghamton.edu>
2025-04-22 18:58:28 -0400

    first commit

---
+++ primGoesShopping.cpp
@@ -0,0 +1,13 @@
+#ifndef _prim_shopping_trip_
+#define _prim_shopping_trip_
+
+#include <vector>
+#inlcude "prim_shopping_trip.h"
+#include <iostream>
+
+using namespace std;
+
+
+int prim_shopping_trip::BubbleUp(vector<int> Q, Node* minNode){
+  
+}
